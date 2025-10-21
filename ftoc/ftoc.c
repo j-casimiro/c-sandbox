20eb108b67adfb243384723938cbe7c4802c71bc
Jehu Casimiro <151370162+j-casimiro@users.noreply.github.com>
2025-10-21 12:19:35 +0000

    added fahrenheit to celsius mini program

---
+++ ftoc/ftoc.c
@@ -0,0 +1,19 @@
+// this code is a reference from the book "The C Programming Language" by Kernighan and Ritchie
+
+#include <stdio.h>
+
+int main() {
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0; // lower limit of temperature scale
+    upper = 300; // upper limit
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%d\t%d\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+}
