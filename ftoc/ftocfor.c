ac569fd56fea531c4842145ba41b006b49879d43
Jehu Casimiro <151370162+j-casimiro@users.noreply.github.com>
2025-10-22 08:51:35 +0000

    added for statement from the book

---
+++ ftoc/ftocfor.c
@@ -0,0 +1,11 @@
+// the for statement 
+
+#include <stdio.h>
+
+int main() {
+    int fahr;
+
+    for (fahr = 0; fahr <= 300; fahr = fahr + 20) {
+        printf("%3d %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+    }
+}
