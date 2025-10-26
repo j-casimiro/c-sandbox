ccfec6ebbf3b5e36ac90b98108c6e2e8fc357933
Jehu Casimiro <151370162+j-casimiro@users.noreply.github.com>
2025-10-26 16:20:31 +0000

    document EOF

--- chario/chario.c
+++ chario/chario.c
@@ -5,6 +5,9 @@ int main() {
     char c;
 
     c = getchar();
+    // EOF means end of file
+    // The EOF value is specifically chosen to be a value 
+    // that cannot represent  any valid character in the character set. 
     while (c != EOF) {
         putchar(c);
         c = getchar();
