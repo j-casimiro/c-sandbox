16d2b1659f0e04a83591676d69527fe02d939282
Jehu Casimiro <151370162+j-casimiro@users.noreply.github.com>
2025-10-27 18:04:13 +0000

    refactor: streamline character input handling in main function

--- chario/chario.c
+++ chario/chario.c
@@ -4,14 +4,17 @@ int main() {
 
     char c;
 
-    c = getchar();
     // EOF means end of file
     // The EOF value is specifically chosen to be a value 
     // that cannot represent  any valid character in the character set. 
-    while (c != EOF) {
-        putchar(c);
-        c = getchar();
-    }
 
+    // The parentheses around the assignment, within the condition are necessary.
+    // The precedence of != is higher than of =,
+    // which means that in the absence of parentheses the relational test !=
+    // would be done before the assignment =.
+    // c = getchar() != EOF is same as c = (getchar() != EOF)
+    while ((c = getchar()) != EOF) {
+        putchar(c);
+    }
     return 0;
 }
