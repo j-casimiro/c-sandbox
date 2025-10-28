c230a1d8751e74199d04b2061b69b823cf5edbda
Jehu Casimiro <151370162+j-casimiro@users.noreply.github.com>
2025-10-28 08:31:30 +0000

    docs: add comments explaining loop behavior and boundary conditions

--- charcounter/nc.c
+++ charcounter/nc.c
@@ -14,5 +14,14 @@ int main() {
         ;
     printf("%.0f\n", nc);
 
+    // one of the nicest things about while and for
+    // is that they test at the top of the loop,
+    // before proceeding with the body.
+    // If there is nothing to do, nothing is done,
+    // even if that means never going through the loop body.
+    // Programs should act intelligently when given zero-length input.
+    // The while and for statements help ensure that programs
+    // do reasonable things with boundary conditions.
+
     return 0;
 }
